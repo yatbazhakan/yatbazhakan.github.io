e0ae539f836749df5f3cee038ca089805877d882
Hakan <yatbazhakan@gmail.com>
2026-04-29 22:57:22 +0100

    update

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/srv/jekyll/src/app/layout":["static/media/e4af272ccee01ff0-s.p.woff2","static/media/7b0b24f36b1a6d0b-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
